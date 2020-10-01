3a175c8ef860e1afac991325af4e4ace0cd05012
Cledson Lopes <cledman@gmail.com>
2020-10-01 18:03:59 -0300

    Frontend implemented.

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,37 +1,30 @@
-import React from 'react'
-import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import React from "react";
+import Input from "../../components/Input";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
-import './styles.css'
+import "./styles.css";
 
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Esses são os proffys disponíveis">
+        <form id="search-teachers">
+          <Input name="subject" label="materia" />
+          <Input name="weel_day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
+        </form>
+      </PageHeader>
 
-function TeacherList () {
-    return(
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Esses são os proffys disponíveis">
-                <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subjec"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="subject">Dia da semana</label>
-                        <input type="text" id="subjec"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>                                                               
-                </form>
-            </PageHeader>
-
-            <main>
-                <TeacherItem/>
-                <TeacherItem/>
-                <TeacherItem/>                                
-            </main>
-        </div>
-    )
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
 }
 
-export default TeacherList
+export default TeacherList;
